dd41fabd55bc98c34112ba0b8ab7dae0e8c2e4c5
Sylvain DELÉPÉE <sylvain.delepee@gmail.com>
2023-09-15 12:49:58 +0200

    Parse literal expression

--- core/src/parser/expression.rs
+++ core/src/parser/expression.rs
@@ -1,21 +1,28 @@
-use crate::lexer::token_stream::{TokenStream};
+use crate::lexer::token_stream::TokenStream;
 
-use super::{indentifier::Identifier, ast_node::{AstNodeData, ParsingResult, Parsable, AstNodePos}, parser::{ParserBuffer, FileAst}, literal::{Literal, self}};
+use super::{
+    ast_node::{AstNodeData, AstNodePos, Parsable, ParsingResult},
+    indentifier::Identifier,
+    literal::Literal,
+    parser::{FileAst, ParserBuffer},
+};
 
 pub enum Expression {
     Identifier(Identifier),
-    Literal(Literal)
+    Literal(Literal),
 }
 
 impl Parsable for Expression {
     fn parse(stream: &mut TokenStream, buffer: &mut ParserBuffer) -> ParsingResult {
         if let ParsingResult::Ok(id) = Identifier::parse(stream, buffer) {
-            return ParsingResult::Ok(id) 
+            return ParsingResult::Ok(id);
         }
 
         if let ParsingResult::Ok(id) = Literal::parse(stream, buffer) {
-            return ParsingResult::Ok(id) 
+            return ParsingResult::Ok(id);
         }
+
+        
 
         ParsingResult::Other
     }
@@ -25,14 +32,14 @@ impl AstNodeData for Expression {
     fn print(&self, file_ast: &FileAst) -> String {
         match self {
             Self::Identifier(id) => id.print(file_ast),
-            Self::Literal(lit) => lit.print(file_ast)
+            Self::Literal(lit) => lit.print(file_ast),
         }
     }
 
     fn get_pos(&self) -> &AstNodePos {
         match self {
             Self::Identifier(id) => id.get_pos(),
-            Self::Literal(lit) => lit.get_pos()
+            Self::Literal(lit) => lit.get_pos(),
         }
     }
 }
